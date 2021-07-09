1571aceede9928d1db53e66bea4aa16cb9099b48
Tom <tomhoag@Gmail.com>
2021-07-09 13:39:56 -0400

    Initial Commit

---
+++ metaspot/metaspotApp.swift
@@ -0,0 +1,17 @@
+//
+//  metaspotApp.swift
+//  metaspot
+//
+//  Created by Tom on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct metaspotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
