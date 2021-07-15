4acdbadd845f1ef22c24d5b0dfd015f0b55e0869
Tom <tomhoag@Gmail.com>
2021-07-15 17:19:43 -0400

    add sample credentials (again)

---
+++ SpotifyApp/SpotifyCredentials.swift
@@ -0,0 +1,15 @@
+//
+//  SpotifyCredentials.swift
+//  SpotifyApp
+//
+//  Created by Tom on 7/15/21.
+//
+
+import Foundation
+
+struct SpotifyCredentials {
+    
+    static let SpotifyClientID = "[Your Spotify ClientID]"
+    static let SpotifyRedirectURL = URL(string: "[Your Spotify Redirect URL]")!
+    
+}
