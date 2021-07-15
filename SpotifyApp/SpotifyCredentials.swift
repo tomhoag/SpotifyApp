47cc8bddd4a2807e643943a46f421b8ef0df8420
Tom <tomhoag@Gmail.com>
2021-07-15 17:12:48 -0400

    add credentials template file

---
+++ SpotifyApp/SpotifyCredentials.swift
@@ -0,0 +1,15 @@
+//
+//  SpotifyCredentials.swift
+//  SpotifyApp
+//
+//  Created by Tom on 7/15/21.
+//
+
+import Foundation
+
+struct SpotifyCredentials {
+    
+    static let SpotifyClientID = "[Your Spotify Client ID]"
+    static let SpotifyRedirectURL = URL(string: "[Your Spotify Redirect URL]")!
+    
+}
